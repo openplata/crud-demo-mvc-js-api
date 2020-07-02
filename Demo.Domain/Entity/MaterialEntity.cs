cb486940126f973ac6bb379f86cfb40e7238686b
openplata <65690119+openplata@users.noreply.github.com>
2020-07-02 01:51:48 -0500

    Add files via upload

---
+++ Demo.Domain/Entity/MaterialEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Domain.Entity
+{
+    public class MaterialEntity
+    {
+        public Int32 Id_Material { get; set; }
+        public string Descripcion { get; set; }
+        public string Estado { get; set; }
+        
+    }
+
+       
+}
